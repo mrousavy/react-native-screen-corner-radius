2f8fa1832d39eb6cefb70144bde1e45c88631d00
Marc Rousavy <marcrousavy@hotmail.com>
2020-12-10 18:35:03 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { NativeModules } from 'react-native';
 
+/**
+ * The current device's screen's corner radius. May also be `0` if the corner radius couldn't be detected.
+ */
 export const ScreenCornerRadius: number =
   NativeModules.ScreenCornerRadius.cornerRadius;
