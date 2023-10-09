65f9174a28da8464c8aa83bd507550843fcde844
Saprykin Semen <s.saprykin@concord.ua>
2023-10-09 16:31:34 +0300

    Android Implementation - created getCornerRadius method

--- android/src/main/java/com/reactnativescreencornerradius/ScreenCornerRadiusModule.kt
+++ android/src/main/java/com/reactnativescreencornerradius/ScreenCornerRadiusModule.kt
@@ -1,24 +1,36 @@
 package com.reactnativescreencornerradius
 
+import android.os.Build
+import android.view.RoundedCorner
+import androidx.annotation.RequiresApi
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import android.view.View
+import com.facebook.react.bridge.*
 
-class ScreenCornerRadiusModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class ScreenCornerRadiusModule(reactContext: ReactApplicationContext) :
+  ReactContextBaseJavaModule(reactContext) {
+  private var rootView: View? = null
 
-    override fun getName(): String {
-        return "ScreenCornerRadius"
+  override fun getName(): String {
+    return "ScreenCornerRadius"
+  }
+
+  // Example method
+  // See https://facebook.github.io/react-native/docs/native-modules-android
+  @ReactMethod(isBlockingSynchronousMethod = true)
+  fun getCornerRadius(): Int {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      rootView = currentActivity?.window?.decorView?.findViewById(android.R.id.content)
+      if (rootView != null) {
+        val corner =
+          rootView?.rootWindowInsets?.getRoundedCorner(RoundedCorner.POSITION_TOP_LEFT)
+        return corner?.radius ?: 0;
+      }
+      return 0
     }
-
-    // Example method
-    // See https://facebook.github.io/react-native/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
-    }
-
-    
+    return 0
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 /**
  * The current device's screen's corner radius. May also be `0` if the corner radius couldn't be detected.
  */
 export const ScreenCornerRadius: number =
-  NativeModules.ScreenCornerRadius.cornerRadius;
+  Platform.OS === 'android'
+    ? NativeModules.ScreenCornerRadius.getCornerRadius()
+    : NativeModules.ScreenCornerRadius.cornerRadius;
 
 /**
  * `true` if the screen has rounded corners, `false` otherwise.
