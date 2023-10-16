fea2d2c46c7d3084699d6e3ada78deebe5f73374
Marc Rousavy <me@mrousavy.com>
2023-10-16 17:48:09 +0200

    Bump to 0.2.0; Android support!

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { NativeModules } from 'react-native';
 /**
  * The current device's screen's corner radius. May also be `0` if the corner radius couldn't be detected.
  */
-export const ScreenCornerRadius: number = NativeModules.ScreenCornerRadius.cornerRadius ?? 0;
+export const ScreenCornerRadius: number =
+  NativeModules.ScreenCornerRadius.cornerRadius ?? 0;
 
 /**
  * `true` if the screen has rounded corners, `false` otherwise.
